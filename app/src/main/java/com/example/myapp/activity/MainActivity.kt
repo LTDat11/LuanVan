cf96b2c434d80d58ca134e06df5369a67a2f3167
LTDat11 <ludat2410@gmail.com>
2024-07-18 10:45:30 +0700

    move to another fragment, set text email curent, toolbar and function sigout

--- app/src/main/java/com/example/myapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapp/activity/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.example.myapp.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.example.myapp.R
+import com.example.myapp.databinding.ActivityMainBinding
+import com.example.myapp.fragment.AccountFragment
+import com.example.myapp.fragment.HomeFragment
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_account -> replaceFragment(AccountFragment())
+            }
+            true
+        }
+
+    }
+
+
+
+    fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, fragment)
+        transaction.commit()
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onBackPressed() {
+        showConfirmExitApp()
+    }
+
+    private fun showConfirmExitApp() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.confirm_exit_app))
+        builder.setPositiveButton(getString(R.string.yes)) { _, _ ->
+            finish()
+        }
+        builder.setNegativeButton(getString(R.string.no)) { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
     }
 }
---
+++ app/src/main/java/com/example/myapp/fragment/AccountFragment.kt
@@ -0,0 +1,71 @@
+package com.example.myapp.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.myapp.R
+import com.example.myapp.activity.LoginActivity
+import com.example.myapp.activity.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class AccountFragment : Fragment() {
+    private var mView: View? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_account, container, false)
+        initToolbar()
+        initUi()
+        initListener()
+        return mView
+    }
+    private fun initListener() {
+        val layoutSignOut = mView?.findViewById<View>(R.id.layout_sign_out)
+        layoutSignOut?.setOnClickListener { confirmSignOut() }
+    }
+
+    private fun confirmSignOut() {
+        val builder = context?.let { androidx.appcompat.app.AlertDialog.Builder(it) }
+        builder?.setTitle(getString(R.string.confirm_sign_out))
+        builder?.setPositiveButton(getString(R.string.yes)) { _, _ -> signOut() }
+        builder?.setNegativeButton(getString(R.string.no)) { dialog, _ -> dialog.dismiss() }
+        builder?.show()
+    }
+
+    //    log out of the app
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(activity, LoginActivity::class.java)
+
+        // Clear the activity stack to prevent the user from going back to this activity after signing out
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        // Start LoginActivity
+        startActivity(intent)
+    }
+
+    private fun initUi() {
+        val tvUsername = mView?.findViewById<TextView>(R.id.tv_username)
+        tvUsername?.text = FirebaseAuth.getInstance().currentUser?.email
+    }
+// set up the toolbar text, back button click listener and set text to Account
+    private fun initToolbar() {
+        val imgToolbarBack = mView?.findViewById<ImageView>(R.id.img_toolbar_back)
+        val tvToolbarTitle = mView?.findViewById<TextView>(R.id.tv_toolbar_title)
+        imgToolbarBack?.setOnClickListener { backToHomeScreen(HomeFragment()) }
+        tvToolbarTitle?.text = getString(R.string.nav_account)
+    }
+
+    private fun backToHomeScreen(fragment: Fragment) {
+        val mainActivity = activity as MainActivity? ?: return
+        mainActivity.replaceFragment(fragment)
+    }
+}
