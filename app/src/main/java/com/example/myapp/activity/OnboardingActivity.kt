35bce80432768045dd717354ebcfb70667b8a46a
LTDat11 <ludat2410@gmail.com>
2024-10-10 17:01:08 +0700

    update onboarding

--- app/src/main/java/com/example/myapp/activity/OnboardingActivity.kt
+++ app/src/main/java/com/example/myapp/activity/OnboardingActivity.kt
@@ -19,20 +19,16 @@ class OnboardingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
 
-        // Tham chiếu ViewPager2
         viewPager = findViewById(R.id.view_pager_on_boarding)
         val adapter = MyViewPagerOnboardingAdapter(this)
         viewPager.adapter = adapter
 
-        // Tham chiếu CircleIndicator3 và kết nối với ViewPager2
         indicator = findViewById(R.id.circle_indicator)
         indicator.setViewPager(viewPager)
 
-        // Tham chiếu layout_next và nút Skip
         layoutNext = findViewById(R.id.layout_next)
         skipButton = findViewById(R.id.text_view_skip)
 
-        // Thay đổi sự kiện click cho layout_next
         layoutNext.setOnClickListener {
             val currentItem = viewPager.currentItem
             if (currentItem < adapter.itemCount - 1) {
@@ -41,12 +37,11 @@ class OnboardingActivity : AppCompatActivity() {
             }
         }
 
-        // Thay đổi sự kiện click cho nút Skip
         skipButton.setOnClickListener {
             viewPager.currentItem = adapter.itemCount - 1
         }
 
-        // Thêm callback để lắng nghe sự thay đổi trang
+        // callback để lắng nghe sự thay đổi trang
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
