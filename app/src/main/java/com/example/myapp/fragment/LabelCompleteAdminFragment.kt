88e6c3fd0ce502412bb081eacd3455ca83959cd5
LTDat11 <ludat2410@gmail.com>
2024-10-11 11:02:18 +0700

    update search by voice for complete admin

--- app/src/main/java/com/example/myapp/fragment/LabelCompleteAdminFragment.kt
+++ app/src/main/java/com/example/myapp/fragment/LabelCompleteAdminFragment.kt
@@ -1,11 +1,17 @@
 package com.example.myapp.fragment
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,13 +23,16 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.Locale
 
 class LabelCompleteAdminFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var orderAminAdapter: OrderAdminAdapter
     private lateinit var noDataTextView: TextView
     private lateinit var searchView: SearchView
+    private lateinit var imageButtonMicrophone: ImageButton
     private val orders = mutableListOf<Order>()
+    private val REQUEST_CODE_VOICE_RECOGNITION = 100
 
 
     override fun onCreateView(
@@ -32,6 +41,7 @@ class LabelCompleteAdminFragment : Fragment() {
     ): View? {
         val mView = inflater.inflate(R.layout.fragment_label_complete_admin, container, false)
         noDataTextView = mView.findViewById(R.id.tv_no_data)
+        imageButtonMicrophone = mView.findViewById(R.id.image_button_microphone)
         recyclerView = mView.findViewById(R.id.rcv_label_complete)
         recyclerView.layoutManager = LinearLayoutManager(context)
         orderAminAdapter = OrderAdminAdapter(orders)
@@ -46,6 +56,41 @@ class LabelCompleteAdminFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpSearchView()
+        initlisteners()
+    }
+
+    private fun initlisteners() {
+        imageButtonMicrophone.setOnClickListener {
+            openVoiceRecognizer()
+        }
+    }
+
+    private fun openVoiceRecognizer() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            putExtra(RecognizerIntent.EXTRA_PROMPT, "Nói điều gì đó...")
+        }
+
+        try {
+            startActivityForResult(intent, REQUEST_CODE_VOICE_RECOGNITION)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(context, "Không hỗ trợ nhận diện giọng nói.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_VOICE_RECOGNITION && resultCode == Activity.RESULT_OK) {
+            val results = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            results?.let {
+                if (it.isNotEmpty()) {
+                    // Điền kết quả vào SearchView
+                    searchView?.setQuery(it[0], false)
+                }
+            }
+        }
     }
 
     private fun setUpSearchView() {
